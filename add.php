f73c8e46c55acd6e25a677bcef509916ee239a81
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-10-24 20:24:18 +0700

    adding validation

--- add.php
+++ add.php
@@ -6,21 +6,30 @@
         if(empty($_POST['email'])){
             echo 'An email is required </br>';
         } else {
-            echo htmlspecialchars($_POST['email']);
+           $email = $_POST['email'];
+           if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                echo 'Email must be a valid email';
+           }
         }
 
         // Check title
         if(empty($_POST['title'])){
             echo 'A title is required </br>';
         } else {
-            echo htmlspecialchars($_POST['title']);
+            $title = $_POST['title'];
+            if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+                echo 'Titles must be letters and space only';
+            }
         }
         
         // Check ingredients
         if(empty($_POST['ingredients'])){
             echo 'At least one ingredient is required </br>';
         } else {
-            echo htmlspecialchars($_POST['ingredients']);
+            $ingredients = $_POST['ingredients'];
+            if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+                echo 'Ingredients must be a comma separated list';
+            }
         }
         
     } // This is the end of post method
