d2088414d47f6129131dd5aa3883fe55e2bbc7ec
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-11-01 21:30:53 +0700

    null coalescing

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,15 @@
+<?php
+
+    session_start();
+
+    if($_SERVER['QUERY_STRING'] == 'noname'){
+        unset($_SESSION['name']);
+    }
+
+    $name = $_SESSION['name'] ?? 'Guest';
+
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -31,6 +43,7 @@
             <div class="container">
                 <a href="index.php" class="brand-logo brand-text">Dom's Pizza</a>
                 <ul id="nav-mobile" class="right hide-on-small-and-down">
+                    <li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
                     <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
                 </ul>
             </div>
