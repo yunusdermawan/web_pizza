880c27345e43ae1ef54780898c48a2d993cf968f
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-11-01 07:23:28 +0700

    adding delete method

--- details.php
+++ details.php
@@ -1,6 +1,19 @@
 <?php
 
     include('config/db_connect.php');
+    if(isset($_POST['delete'])){
+        $id_to_del = mysqli_real_escape_string($conn, $_POST['id_to_del']);
+
+        $sql = "DELETE FROM pizzas WHERE id = $id_to_del";
+
+        if(mysqli_query($conn, $sql)){
+            // Success
+            header('Location: index.php');
+        } {
+            // Failure
+            echo 'query error:' . mysqli_error($conn);
+        }
+    }
 
     // Check GET req id param
     if(isset($_GET['id'])){
@@ -37,6 +50,12 @@
         <p><?php echo date($pizza['created_at']); ?></p>
         <h5>Ingredients:</h5>
         <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+        <!-- DELETE FORM -->
+        <form action="details.php" method="post">
+            <input type="hidden" name="id_to_del" value="<?php echo $pizza['id']; ?>">
+            <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+        </form>
 
     <?php else: ?>
         <h5>No such pizza exists!</h5>
